59d7e640e1e0f28790eba612a7a63679535d4260
JoseGarciaMayen <josegarciamayen@gmail.com>
2026-06-16 18:22:10 +0200

    fix: changed to ms-marco-MiniLM-L-6-v2 to try and host demo in render

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
 from sentence_transformers import CrossEncoder, SentenceTransformer
 
 EMBED_MODEL = SentenceTransformer("all-MiniLM-L6-v2")
-RERANK_MODEL = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-12-v2")
+RERANK_MODEL = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
